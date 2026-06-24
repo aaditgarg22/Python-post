eba21458d9f2e06c81c03c826fcea8627f0fa163
aaditgarg22 <gargaadit22@gmail.com>
2026-06-24 18:41:49 +0530

    string operations

--- python/firstpython.py
+++ python/firstpython.py
@@ -1,6 +1,9 @@
 #consedering a rectangle with side a and b
-a=int(input("Enter the length of side a: "))
-b=int(input("Enter the length of side b: "))
-name='rectangle'
-print ("The area of the", name, "is", a*b) 
-print("The perimeter of the", name, "is", 2*(a+b))
+#a=int(input("Enter the length of side a: "))
+#b=int(input("Enter the length of side b: "))
+#name='rectangle'
+#print ("The area of the", name, "is", a*b) 
+#print("The perimeter of the", name, "is", 2*(a+b))
+
+str = input("Enter a string: ")
+print("The length of the string is: ", len(str)) 
