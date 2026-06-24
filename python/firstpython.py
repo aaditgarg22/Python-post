1608809bbce3a938e04cd40d66f5d27f4f3260c4
aaditgarg22 <gargaadit22@gmail.com>
2026-06-24 19:20:12 +0530

    conditional statements

--- python/firstpython.py
+++ python/firstpython.py
@@ -1,14 +1,22 @@
-#consedering a rectangle with side a and b
-#a=int(input("Enter the length of side a: "))
-#b=int(input("Enter the length of side b: "))
-#name='rectangle'
-#print ("The area of the", name, "is", a*b) 
-#print("The perimeter of the", name, "is", 2*(a+b))
+"""consedering a rectangle with side a and b
+a=int(input("Enter the length of side a: "))
+b=int(input("Enter the length of side b: "))
+name='rectangle'
+print ("The area of the", name, "is", a*b) 
+print("The perimeter of the", name, "is", 2*(a+b))
 
-str = input("Enter a string: ")
-print("The length of the string is: ", len(str)) 
+str1 = input("Enter a string: ")
+str= "aadit garg"\
+print("The length of the string is: ", len(str1)) 
 print (str.replace("a", "Q"))
 print (str.capitalize())
 print(str.count("garg"))
 print(str.find("d"))
-print(str.endswith("rg"))
+print(str.endswith("rg"))"""
+
+age= int(input("your age="))
+if( age>=18):
+    print("u are an adult")
+else:
+    print("u are not an adult")
+    
