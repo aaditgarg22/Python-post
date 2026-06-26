216db65e400980f331d10c480db7920f494552b4
aaditgarg22 <aaditgarg239@gmail.com>
2026-06-27 02:16:08 +0530

    palindrome

--- QuestionSolving.py
+++ QuestionSolving.py
@@ -1,7 +1,20 @@
-a=input("enter your 1st film: ")
+"""a=input("enter your 1st film: ")
 b=input("enter your 2nd film: ")
 c=input("enter your 3rd film: ")
 
 list=[a,b,c]
 
-print(list)
+print(list)"""
+
+list=[1,2,3]
+list2=[1,2,3,2,1]
+
+listcopy=list2.copy()
+listcopy.reverse()
+
+if(listcopy==list2):
+    print("palindrome")
+else:
+    print("not palindrome")
+
+    
